c0a5a2ec246d17009d255efddd2bbb38af18a5eb
vltmn <melkerveltman@gmail.com>
2021-12-01 11:59:26 +0100

    Added day1 and main

---
+++ scala/src/main/scala/example/Main.scala
@@ -0,0 +1,22 @@
+package io.vltmn.aoc2021
+package example
+
+import scala.io.Source
+
+class InputReader(filename: String) {
+  def read(): String = {
+    val src = Source.fromFile(getClass.getResource("/" + filename).getFile)
+    val data = src.mkString
+    src.close()
+    data
+  }
+}
+
+object Main extends App {
+  val solution: Solution = new Day1()
+  val reader = new InputReader(solution.getClass.getSimpleName.toLowerCase())
+  val data = reader.read()
+  val output = solution.solve(data)
+
+  println(s"Output was:\n${output}")
+}
---
+++ scala/src/test/scala/example/Day1Test.scala
@@ -0,0 +1,16 @@
+package io.vltmn.aoc2021
+package example
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class Day1Test extends AnyFunSuite {
+  val day1 = new Day1()
+  val demoInput = "199\n200\n208\n210\n200\n207\n240\n269\n260\n263";
+  test("Demo input part A") {
+    assert(day1.partA(demoInput) == 7)
+  }
+  test("Demo input part B") {
+    assert(day1.partB(demoInput) == 5)
+  }
+
+}
