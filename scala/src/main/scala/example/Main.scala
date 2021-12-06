f98151285e73779746116d609a0f565612a22d70
vltmn <melkerveltman@gmail.com>
2021-12-06 14:04:16 +0100

    Added day6

--- scala/src/main/scala/example/Main.scala
+++ scala/src/main/scala/example/Main.scala
@@ -13,7 +13,7 @@ class InputReader(filename: String) {
 }
 
 object Main extends App {
-  val solution: Solution = new Day5()
+  val solution: Solution = new Day6()
   val reader = new InputReader(solution.getClass.getSimpleName.toLowerCase())
   val data = reader.read()
   val output = solution.solve(data)
