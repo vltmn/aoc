d0bd73e9f64275742e39953d23cb4d1abc8a4a2e
vltmn <melkerveltman@gmail.com>
2022-12-10 14:47:34 +0900

    add a default thread to run solutions in to be able to time them out easier

--- scala/src/main/scala/Main.scala
+++ scala/src/main/scala/Main.scala
@@ -2,14 +2,19 @@ package io.vltmn.aoc
 
 import util.InputReader
 
+import scala.concurrent.{Await, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.{Duration, SECONDS}
+
 object Main extends App {
   val solution: Solution = new y2022.Day10()
   val day: Int = solution.getClass.getSimpleName.substring(3).toInt
   val year = solution.getClass.getPackageName.split("\\.").last.substring(1).toInt
   val reader = new InputReader(year, day)
   val data = reader.read()
-  val pre = System.nanoTime();
-  val output = solution.solve(data)
+  val solveFuture = Future(solution.solve(data))
+  val pre = System.nanoTime()
+  val output = Await.result(solveFuture, Duration(7, SECONDS))
   val execTime = System.nanoTime() - pre
   println(s"Output was:\n${output}")
   val execTimeFormatted = execTime.toDouble / Math.pow(10, 9)
