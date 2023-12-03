54e728a2cb51676c5736b740ad5ffba0589405c8
vltmn <melkerveltman@gmail.com>
2023-12-03 14:54:21 +0100

    tmp

---
+++ kotlin/src/main/kotlin/y2023/Day3.kt
@@ -0,0 +1,22 @@
+package y2023
+
+import Solution
+
+class Day3: Solution {
+    override fun solve(input: String): String {
+        val inp2 = "467..114..\n" +
+                "...*......\n" +
+                "..35..633.\n" +
+                "......#...\n" +
+                "617*......\n" +
+                ".....+.58.\n" +
+                "..592.....\n" +
+                "......755.\n" +
+                "...\$.*....\n" +
+                ".664.598.."
+
+        val p1 = ""
+        val p2 = ""
+        return "Part1: $p1\nPart2: $p2"
+    }
+}
